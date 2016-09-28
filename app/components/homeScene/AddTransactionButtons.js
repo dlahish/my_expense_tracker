515243da884d3221cf0b0df3c9f45d7d36564bcf
Nadav Lachish <d_lahish@walla.com>
2016-09-28 15:42:29 +0200

    change style for iphone4

--- app/components/homeScene/AddTransactionButtons.js
+++ app/components/homeScene/AddTransactionButtons.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import Button from 'react-native-button'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -40,6 +40,10 @@ export default AddTransactionButtons = (props) => {
   )
 }
 
+AddTransactionButtons.propTypes = {
+  setCategoryType: PropTypes.func.isRequired
+}
+
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row'
@@ -51,7 +55,7 @@ const styles = StyleSheet.create({
   },
   btnText: {
 		color: 'black',
-    fontSize: 20,
+    fontSize: 15,
     paddingLeft: 10
 	},
 	btn: {
