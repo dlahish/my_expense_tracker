3bf7e71133e5abd23864b78f1d91687a7fc21698
Nadav Lachish <d_lahish@walla.com>
2016-10-10 09:01:47 +0200

    create categories folder in components

--- app/components/index.js
+++ app/components/index.js
@@ -1,39 +1,37 @@
-import Home from './Home'
-import GuestActions from './GuestActions'
+import Home from './homeScene/Home'
 import Signin from './authentication/Signin'
 import SigninForm from './authentication/SigninForm'
 import SignupForm from './authentication/SignupForm'
 import Signup from './authentication/Signup'
-import UserActions from './UserActions'
-import TabIcon from './TabIcon'
-import TabView from './TabView'
-import NewTransaction from './newTransaction/NewTransaction'
-import NewTransactionForm from './newTransaction/NewTransactionForm'
-import CurrentMonthTotal from './CurrentMonthTotal'
+import TabIcon from './common/TabIcon'
+import NewTransaction from './transactions/NewTransaction'
+import NewTransactionForm from './transactions/NewTransactionForm'
+import CurrentMonthTotal from './homeScene/CurrentMonthTotal'
 import addBorder from './addBorder'
-import PresetTransactions from './PresetTransactions'
+import PresetTransactions from './settings/PresetTransactions'
 import AddTransactionButtons from './homeScene/AddTransactionButtons'
 import Button from './common/Button'
 import DatePicker from './common/DatePicker'
-import DatePickerModal from './DatePickerModal'
-import CategoryList from './CategoryList'
-import Transactions from './Transactions'
-import Categories from './Categories'
+import DatePickerModal from './common/DatePickerModal'
+import CategoryList from './categories/CategoryList'
+import Transactions from './transactions/Transactions'
+import Categories from './categories/Categories'
 import CustomNavBar from './common/CustomNavBar'
-import NewCategory from './NewCategory'
-import NewCategoryForm from './NewCategoryForm'
-import Settings from './Settings'
+import NewCategory from './categories/NewCategory'
+import NewCategoryForm from './categories/NewCategoryForm'
+import Settings from './settings/Settings'
 import ItemRow from './common/ItemRow'
 import CategorySelector from './common/CategorySelector'
-import CurrencySymbols from './CurrencySymbols'
+import CurrencySymbols from './settings/CurrencySymbols'
 import ChangeMonthArrows from './homeScene/ChangeMonthArrows'
 import DisplayFavoriteTransactions from './homeScene/DisplayFavoriteTransactions'
 import MessageModal from './common/MessageModal'
 import Loader from './common/Loader'
 import ListItem from './common/ListItem'
-import MonthHeader from './MonthHeader'
+import MonthHeader from './common/MonthHeader'
 import MenuModal from './common/MenuModal'
-import FilteredAndSortedTransactionsTotal from './FilteredAndSortedTransactionsTotal'
+import FilteredAndSortedTransactionsTotal from './transactions/FilteredAndSortedTransactionsTotal'
+import Export from './transactions/Export'
 
 export {
   Home,
@@ -41,10 +39,7 @@ export {
   SigninForm,
   SignupForm,
   Signup,
-  GuestActions,
-  UserActions,
   TabIcon,
-  TabView,
   NewTransaction,
   CurrentMonthTotal,
   addBorder,
@@ -71,5 +66,6 @@ export {
   ListItem,
   MonthHeader,
   MenuModal,
-  FilteredAndSortedTransactionsTotal
+  FilteredAndSortedTransactionsTotal,
+  Export
 }
