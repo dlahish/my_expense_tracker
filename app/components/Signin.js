dlahish/spendingapp2

add authError to signin form

app/components/Signin.js
@@ -7,17 +7,21 @@ import * as accountActionCreators from '../actions/accounts'
 
 function Signin (props) {
 
-	handleFormSubmit = (credentials) => {
+	handleFormSubmit = (credentials, authError) => {
+		console.log('handleFormSubmit ----')
+		console.log('authError - ' + authError)
 		props.signinAndAuthUser(credentials)
-			.then(() => Actions.home())
+			.then((authError) => {
+				if (!authError) Actions.home()
+			})
 	}
 
   return (
-    <AccountForm onSubmit={handleFormSubmit}/>
+    <AccountForm onSubmit={handleFormSubmit} authError={props.authError}/>
   )
 }
 
 export default connect(
-	(state) => ({}),
+	(state) => ({ authError: state.account.user.authError }),
 	(dispatch) => (bindActionCreators(accountActionCreators, dispatch))
 )(Signin)

dlahish/my_expense_tracker

app/components/Signin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AccountForm } from '../components'
+import { SigninForm } from '../components'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Actions } from 'react-native-router-flux'
@@ -7,17 +7,21 @@ import * as accountActionCreators from '../actions/accounts'
 
 function Signin (props) {
 
-	handleFormSubmit = (credentials) => {
+	handleFormSubmit = (credentials, authError) => {
+		console.log('handleFormSubmit ----')
+		console.log('authError - ' + authError)
 		props.signinAndAuthUser(credentials)
-			.then(() => Actions.home())
+			.then((authError) => {
+				if (!authError) Actions.home()
+			})
 	}
 
   return (
-    <AccountForm onSubmit={handleFormSubmit}/>
+    <SigninForm onSubmit={handleFormSubmit} authError={props.authError}/>
   )
 }
 
 export default connect(
-	(state) => ({}),
+	(state) => ({ authError: state.account.user.authError }),
 	(dispatch) => (bindActionCreators(accountActionCreators, dispatch))
 )(Signin)