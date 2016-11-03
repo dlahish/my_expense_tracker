dlahish/spendingapp2

add redux-persist

app/api/accounts.js
@@ -4,40 +4,56 @@ import { AsyncStorage } from 'react-native'
 const BASE_URI = `https://spendingapi2.herokuapp.com`
 // const BASE_URI = `http://192.168.0.104:3090`
 
-export function checkAuth() {
-  console.log('CHECK AUTH ACTION ---')
-  return AsyncStorage.getItem('spending-user-token')
-}
+// export function checkAuth() {
+//   return AsyncStorage.getItem('spending-user-token')
+// }
+
+// export function checkAuth(state) {
+//   console.log('checkauth--------------')
+//   console.log(state)
+//   console.log(state.account.token)
+//   return state.account.token
+// }
+
+// export function currentUser(token) {
+//   if (token === null) return {}
+//   return axios.get(`${BASE_URI}/getemail`, { headers: { authorization: token }})
+// }
 
 export function currentUser(token) {
-  console.log('CURRENT USER ACTION ---')
   if (token === null) return {}
   return axios.get(`${BASE_URI}/getemail`, { headers: { authorization: token }})
 }
 
 export function signin (credentials) {
-  console.log('SIGN IN ACTION ---')
   const email = credentials.email, password = credentials.password
 	return axios.post(`${BASE_URI}/signin`, { email, password })
 }
 
 export function signup(credentials) {
-  console.log('SIGN UP ACTION ---')
   const email = credentials.email, password = credentials.password
 	return axios.post(`${BASE_URI}/signup`, { email, password })
 }
 
+// export function setAuth (res) {
+//   if (res.data.message) {
+//     return { authError: res.data.message }
+//   } else {
+//     AsyncStorage.setItem('spending-user-token', res.data.token)
+//     return res
+//   }
+// }
+
 export function setAuth (res) {
-  console.log('SET AUTH ACTION ---')
-  console.log(res.data)
   if (res.data.message) {
     return { authError: res.data.message }
   } else {
-    AsyncStorage.setItem('spending-user-token', res.data.token)
+    return { token: red.data.token }
     return res
   }
 }
 
-export function revokeAuth () {
-	return AsyncStorage.removeItem('spending-user-token')
-}
+
+// export function revokeAuth () {
+// 	return AsyncStorage.removeItem('spending-user-token')
+// }

dlahish/my_expense_tracker

app/api/accounts.js
@@ -4,40 +4,56 @@ import { AsyncStorage } from 'react-native'
 const BASE_URI = `https://spendingapi2.herokuapp.com`
 // const BASE_URI = `http://192.168.0.104:3090`
 
-export function checkAuth() {
-  console.log('CHECK AUTH ACTION ---')
-  return AsyncStorage.getItem('spending-user-token')
-}
+// export function checkAuth() {
+//   return AsyncStorage.getItem('spending-user-token')
+// }
+
+// export function checkAuth(state) {
+//   console.log('checkauth--------------')
+//   console.log(state)
+//   console.log(state.account.token)
+//   return state.account.token
+// }
+
+// export function currentUser(token) {
+//   if (token === null) return {}
+//   return axios.get(`${BASE_URI}/getemail`, { headers: { authorization: token }})
+// }
 
 export function currentUser(token) {
-  console.log('CURRENT USER ACTION ---')
   if (token === null) return {}
   return axios.get(`${BASE_URI}/getemail`, { headers: { authorization: token }})
 }
 
 export function signin (credentials) {
-  console.log('SIGN IN ACTION ---')
   const email = credentials.email, password = credentials.password
 	return axios.post(`${BASE_URI}/signin`, { email, password })
 }
 
 export function signup(credentials) {
-  console.log('SIGN UP ACTION ---')
   const email = credentials.email, password = credentials.password
 	return axios.post(`${BASE_URI}/signup`, { email, password })
 }
 
+// export function setAuth (res) {
+//   if (res.data.message) {
+//     return { authError: res.data.message }
+//   } else {
+//     AsyncStorage.setItem('spending-user-token', res.data.token)
+//     return res
+//   }
+// }
+
 export function setAuth (res) {
-  console.log('SET AUTH ACTION ---')
-  console.log(res.data)
   if (res.data.message) {
     return { authError: res.data.message }
   } else {
-    AsyncStorage.setItem('spending-user-token', res.data.token)
+    return { token: res.data.token }
     return res
   }
 }
 
-export function revokeAuth () {
-	return AsyncStorage.removeItem('spending-user-token')
-}
+
+// export function revokeAuth () {
+// 	return AsyncStorage.removeItem('spending-user-token')
+// }